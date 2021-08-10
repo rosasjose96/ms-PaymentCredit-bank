e5e5898fc546f496b66fe5aea766c1866c8613a8
rosasjose96 <jose_alonso_rosas@outlook.com>
2021-08-10 11:31:25 -0500

    FEAT modify logic port

--- src/main/java/com/bootcamp/msPaymentCredit/services/impl/TransactionActiveServiceImpl.java
+++ src/main/java/com/bootcamp/msPaymentCredit/services/impl/TransactionActiveServiceImpl.java
@@ -26,7 +26,7 @@ public class TransactionActiveServiceImpl implements ITransactionDTOService {
         LOGGER.info("initializing Transaction create");
 
         return client
-                .baseUrl("http://localhost:8087/api/transaction")
+                .baseUrl("http://localhost:8095/api/transaction")
                 .build()
                 .post()
                 .accept(MediaType.APPLICATION_JSON)
